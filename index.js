7b4174ea76e5d8d7ab118ca0c93e288a5fca9ca4
Albert Hao <alberthao@Authorized-SMUHSD-Device.local>
2018-05-14 21:59:55 -0700

    m

--- index.js
+++ index.js
@@ -188,12 +188,12 @@ function startServer() {
 
 	});
 
-	app.get('/bb', (req, res, next) => {
-		if(!req.user) res.redirect('/login');
-		var filePath = path.join(__dirname, './public/game/bb.html')
-		res.sendFile(filePath);
-
-	});
+	// app.get('/bb', (req, res, next) => {
+	// 	if(!req.user) res.redirect('/login');
+	// 	var filePath = path.join(__dirname, './public/game/bb.html')
+	// 	res.sendFile(filePath);
+	//
+	// });
 
 	/* Defines what function to all when the server recieves any request from http://localhost:8080 */
 	server.on('listening', () => {
