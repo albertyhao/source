432a00a02268474e615700527542c37003bfb4ec
Amanda Hao <amandahao@gmail.com>
2018-05-22 21:33:48 -0700

    walking

--- public/js/game.js
+++ public/js/game.js
@@ -5,6 +5,35 @@ var bulletImage = new Image();
 var bullets = [];
 var bulletSpeed = 500;
 var bulletWidth = 5;
+var direction = 'right';
+var index = 0;
+var Sprite = function(name) {
+  this.front = [new Image(), new Image(), new Image()];
+  this.left = [new Image(), new Image(), new Image()];
+  this.right = [new Image(), new Image(), new Image()];
+  this.back = [new Image(), new Image(), new Image()];
+
+  this.front[0].src = `/${name}sprites/${name}front1.png`
+  this.front[1].src = `/${name}sprites/${name}front2.png`
+  this.front[2].src = `/${name}sprites/${name}front3.png`
+
+  this.left[0].src = `/${name}sprites/${name}left1.png`
+  this.left[1].src = `/${name}sprites/${name}left2.png`
+  this.left[2].src = `/${name}sprites/${name}left3.png`
+
+  this.right[0].src = `/${name}sprites/${name}right1.png`
+  this.right[1].src = `/${name}sprites/${name}right2.png`
+  this.right[2].src = `/${name}sprites/${name}right3.png`
+
+  this.back[0].src = `/${name}sprites/${name}back1.png`
+  this.back[1].src = `/${name}sprites/${name}back2.png`
+  this.back[2].src = `/${name}sprites/${name}back3.png`
+}
+var sprites = {
+  dean: new Sprite('dean'),
+  sam: new Sprite('sam'),
+  cas: new Sprite('cas')
+}
 bulletImage.src = '/img/clash2.png'
 
 socket.on('playerUpdate', updatePlayers);
@@ -49,11 +78,11 @@ function createNewPlayer(playerName) {
 function drawPlayers() {
 
  var playerNames = Object.keys(gamePieces);
- var pieceWidth = Math.min($canvas.width, $canvas.height) / 5;
+ var pieceWidth = Math.min($canvas.width, $canvas.height) / 10;
  playerNames.forEach(function(playerName) {
    var gamePiece = gamePieces[playerName];
    if(!gamePiece.loaded) return;
-   context.drawImage(gamePiece.picture ,gamePiece.x, gamePiece.y, pieceWidth, pieceWidth);
+   context.drawImage(sprites[playerName][direction][index % 3] ,gamePiece.x, gamePiece.y, pieceWidth, pieceWidth);
  });
 
 }
@@ -121,19 +150,31 @@ function fire(){
 function handlePlayerAction(e) {
 
  var gamePiece = gamePieces[user];
- var step = 50;
+ var step = 25;
  switch(e.key) {
    case 'ArrowLeft':
+   case 'a':
      gamePiece.x-= step;
+     direction = 'left';
+     index++;
      break;
    case 'ArrowRight':
+   case 'd':
      gamePiece.x+= step;
+     direction = 'right';
+     index++;
      break;
    case 'ArrowDown':
+   case 's':
      gamePiece.y+= step;
+     direction = 'front';
+     index++;
      break;
    case 'ArrowUp':
+   case 'w':
      gamePiece.y-= step;
+     direction = 'back';
+     index++;
      break;
    case 'p':
      fire();
